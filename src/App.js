534d1097c113b1f485481596a8ffb780b23c973d
randomDiamond <yodajoe@web.de>
2022-11-06 15:17:06 +0100

    main program

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import Start from './Start.js'
+import Main from './Main.js';
 import './App.css';
 
 function App() {
+  
+  const [guessWord, setGuessWord] = useState('')
+  const [gameStarted, setGameStarted] = useState(false)
+  function handleChange(event){
+    setGuessWord(event.target.value)
+    {console.log(guessWord)}
+  }
+  function startGame(){
+    console.log(gameStarted)
+    setGameStarted(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    {!gameStarted && <Start startGame={startGame} guessWord={guessWord} handleChange={handleChange}/>}
+    {gameStarted && <Main guessWord={guessWord}/>}
+    
+    </>
   );
 }
 
